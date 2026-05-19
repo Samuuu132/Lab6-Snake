69e374bbb40910813b3b2bb79919711a3d4bc1f2
Samuuu132 <gud24451@ug.edu.gt>
2026-05-19 17:55:02 -0600

    simplificar el componente App y añadir el componente Board

---
+++ src/components/Board.jsx
@@ -0,0 +1,14 @@
+function Board() {
+  return (
+    <div style={{
+      width: '400px',
+      height: '400px',
+      backgroundColor: '#12121f',
+      border: '2px solid rgba(0, 255, 136, 0.3)',
+      position: 'relative',
+    }}>
+    </div>
+  )
+}
+
+export default Board
